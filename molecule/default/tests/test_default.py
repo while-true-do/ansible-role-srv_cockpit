0b8bf2f8bbd705da3cdc7f33ba02d424803fb8d8
daniel-wtd <daniel@while-true-do.io>
2019-05-23 12:15:12 +0200

    fix: testing broken

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,17 +15,42 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     ("cockpit-ws"),
     ("cockpit-dashboard"),
 ])
-def test_pkg(host, name):
+def test_cockpit_pkg(host, name):
     pkg = host.package(name)
+
     assert pkg.is_installed
 
 
-def test_srv(host):
+def test_cockpit_web_srv(host):
     srv = host.service('cockpit.socket')
+
     assert srv.is_running
     assert srv.is_enabled
 
 
-def test_socket(host):
+def test_cockpit_web_conf(host):
+    file = host.file('/etc/cockpit/cockpit.conf')
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.contains('192.168.0.10')
+    assert file.contains('host2')
+    assert file.contains('host3.example.com')
+
+
+def test_cockpit_web_socket(host):
     sock = host.socket('tcp://9090')
+
     assert sock.is_listening
+
+
+def test_cockpit_web_clients(host):
+    file = host.file('/etc/cockpit/machines.d/99-webui.json')
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.contains('192.168.0.10')
+    assert file.contains('host2')
+    assert file.contains('host3.example.com')
